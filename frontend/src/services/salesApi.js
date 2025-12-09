a5b1ade4abe06e3c5bf7d37dc69ab27347e51219
Manpreet4491 <ms7454696@gmail.com>
2025-12-10 00:46:00 +0530

    Revert "Call GET /api/sales with query params"
    This reverts commit 58c12e4699cdadf22fcfa79e18d79f579c6ef2c3.

---
+++ frontend/src/services/salesApi.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+});
+
+export async function fetchSales(params) {
+  const response = await api.post("/api/sales/search", params);
+  return response.data;
+}
